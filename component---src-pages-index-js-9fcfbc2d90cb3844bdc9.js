06d49c28e329890c4f928089e677f72e966556e2
Vidushan CHOORIYAKUMARAN <mail@vidu.sh>
2019-10-09 23:12:19 +0200

    Updates

---
+++ component---src-pages-index-js-9fcfbc2d90cb3844bdc9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{188:function(n,e,o){"use strict";o.r(e);var t=o(0),l=o.n(t);e.default=function(){return l.a.createElement("div",null,"Hello world!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-9fcfbc2d90cb3844bdc9.js.map
