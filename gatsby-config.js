e2f04d155d984cbfe0157052c5c71594c6390038
Vidushan CHOORIYAKUMARAN <vidushan.chooriyakumaran@hotmail.fr>
2019-10-10 22:41:48 +0200

    Add bulma + markdown blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Vidushan CHOORIYAKUMARAN`,
+    description: `About me.`,
+    author: `@vidushan_`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -12,9 +12,16 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog-md`,
+        path: `${__dirname}/src/blog-md`,
+      },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
---
+++ src/blog-md/post-1.md
@@ -0,0 +1,7 @@
+---
+path: "/blog/not-your-business"
+date: "2019-10-10"
+title: "Hello world!"
+---
+
+Small test. How did you manage to reach this page? :joy:
