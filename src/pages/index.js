4586e7a0ed199be8f675c5bc3f61f7c6f115fe9b
Vidushan CHOORIYAKUMARAN <vidushan.chooriyakumaran@hotmail.fr>
2019-10-10 23:41:10 +0200

    Add particles.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,34 @@
 import React from "react"
+import Particles from 'react-particles-js';
+
 import { Link } from "gatsby"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 import "./style.scss"
 
-const IndexPage = () => (
-  <div>
-    <SEO title="Home" />
-    <section className="hero is-success is-fullheight">
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <h1 className="title" >Hi there!</h1>
-          <p>Vidushan CHOORIYAKUMARAN, Software Engineer.</p>
+const IndexPage = () => {
+  const particlesParams = {
+    particles: {
+      number: {
+        value: 160,
+      },
+    },
+  }
+  return (
+    <div>
+      <SEO title="Home" />
+      <section className="hero is-success is-fullheight" id="particle-bg">
+        <Particles params={particlesParams} />
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <h1 className="title" >Hi there!</h1>
+            <p>Vidushan CHOORIYAKUMARAN, Software Engineer.</p>
+          </div>
         </div>
-      </div>
-    </section>
-  </div>
-)
+      </section>
+    </div>
+  )
+}
 
 export default IndexPage
