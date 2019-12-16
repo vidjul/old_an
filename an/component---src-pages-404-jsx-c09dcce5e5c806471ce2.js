c820f42a02aac53910f55a71d73a4ca7e54ef35e
Vidushan CHOORIYAKUMARAN <mail@vidu.sh>
2019-12-17 00:33:36 +0100

    Updates

---
+++ an/component---src-pages-404-jsx-c09dcce5e5c806471ce2.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{pssB:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),s=n.n(a),u=n("dmsj");t.default=function(){return s.a.createElement("div",null,s.a.createElement(u.a,{title:"404: Not found"}),s.a.createElement("h1",null,"NOT FOUND"),s.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-c09dcce5e5c806471ce2.js.map
---
+++ an/styles-cee6fa66b34c8b48f8e2.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{r5nU:function(n,w,o){}}]);
+//# sourceMappingURL=styles-cee6fa66b34c8b48f8e2.js.map
