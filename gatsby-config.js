80795c2eb4bfcd8978fe2ed4182ef972370deb9f
Vidushan CHOORIYAKUMARAN <mail@vidu.sh>
2019-12-16 21:34:32 +0100

    Update address bar color

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,11 +25,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        name: `vidushan-personal-page`,
+        short_name: `vidushan`,
+        start_url: `/an`,
+        background_color: `#6fbedb`,
+        theme_color: `#6fbedb`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
@@ -45,4 +45,4 @@ module.exports = {
     // `gatsby-plugin-offline`,
   ],
   pathPrefix: "/an",
-}
+};
