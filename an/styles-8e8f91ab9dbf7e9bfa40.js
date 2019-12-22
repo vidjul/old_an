ea50dce0fde58ef9dd578158c4200e50b3396892
Vidushan CHOORIYAKUMARAN <mail@vidu.sh>
2019-12-22 16:27:03 +0100

    Updates

---
+++ an/styles-8e8f91ab9dbf7e9bfa40.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{r5nU:function(n,o,p){n.exports={dark:"#333745",light:"#05aec0"}}}]);
+//# sourceMappingURL=styles-8e8f91ab9dbf7e9bfa40.js.map
